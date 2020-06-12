78d9c0e155d3322a704bdda68b59368a4eb9fc1d
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-06-13 00:29:05 +0900

    ログインURLのコールバック検証用APIエンドポイントを作成

--- api/controller/auth_controller.go
+++ api/controller/auth_controller.go
@@ -29,3 +29,26 @@ func (controller *AuthController) Issue(c echo.Context) error {
 	resp.Message = url
 	return c.JSON(http.StatusOK, &resp)
 }
+
+func (controller *AuthController) Callback(c echo.Context) error {
+	resp := model.AuthCallbackResponse{}
+
+	req := model.AuthCallbackRequest{}
+	if err := c.Bind(&req); err != nil {
+		resp.Message = "不正なデータです。"
+		return c.JSON(http.StatusBadRequest, &resp)
+	}
+
+	if err := validate.Struct(&req); err != nil {
+		resp.Message = "不正なデータです。"
+		return c.JSON(http.StatusBadRequest, &resp)
+	}
+
+	if err := model.VerifyState(c, req.State); err != nil {
+		resp.Message = "stateが不正です。"
+		return c.JSON(http.StatusBadRequest, &resp)
+	}
+
+	model.StoreCode(c, req.Code)
+	return c.JSON(http.StatusOK, &resp)
+}
